484d519d475dd61dbb79ab6910c59dc1fce1d7e9
Anastasia <anastasia9297@gmail.com>
2021-09-30 22:40:19 +0300

    updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Htag, Button } from '../components';
+import { Htag, Button, Paragraph, Tag } from '../components';
 
 export default function Home(): JSX.Element {
   return (
@@ -6,6 +6,12 @@ export default function Home(): JSX.Element {
       <Htag tag="h1">some text</Htag>
       <Button appearance="primary" arrow="down">button primary</Button>
       <Button appearance="ghost" arrow="right">button ghost</Button>
+
+      <Paragraph size="s">Paragraph 1</Paragraph>
+      <Paragraph size="m">Paragraph 2</Paragraph>
+      <Paragraph size="l">Paragraph 3</Paragraph>
+      <Tag color="green" size="s">Paragraph 3</Tag>
+      <Tag color="red" size="m">Paragraph 3</Tag>
     </>
   );
 }
